3d7853e945840943ce1374309338fccb7d49a44e
jerensl <54782057+jerensl@users.noreply.github.com>
2022-05-11 14:07:19 +0800

    adding reverse proxy and hello world service

--- internal/hello-world-service/main.go
+++ internal/hello-world-service/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello world")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
--- internal/reverse-proxy/main.go
+++ internal/reverse-proxy/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
 func main() {
-	
+	demoUrl, err := url.Parse("http://localhost:8080")
+	if err != nil {
+		panic(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(demoUrl)
+	http.ListenAndServe(":8081", proxy)
 }
