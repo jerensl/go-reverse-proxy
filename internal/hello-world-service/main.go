db90ad78b97f4e2b383498ce4de7b788f9384c4b
jerensl <54782057+jerensl@users.noreply.github.com>
2022-05-11 15:37:08 +0800

    adding development server

--- internal/hello-world-service/main.go
+++ internal/hello-world-service/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Req: %s %s \n", r.Host, r.URL.Path) 
+
 		fmt.Fprint(w, "Hello world")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
--- internal/reverse-proxy/main.go
+++ internal/reverse-proxy/main.go
@@ -4,14 +4,27 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 )
 
 func main() {
-	demoUrl, err := url.Parse("http://localhost:8080")
+	origin, err := url.Parse(os.Getenv("HELLO_WORLD_SERVICE_ADDR"))
 	if err != nil {
 		panic(err)
 	}
+	director := func(req *http.Request) {
+		req.Header.Add("X-Forwarded-Host", req.Host)
+		req.Header.Add("X-Origin-Host", origin.Host)
+		req.URL.Scheme = "http"
+		req.URL.Host = origin.Host
+	}
 
-	proxy := httputil.NewSingleHostReverseProxy(demoUrl)
-	http.ListenAndServe(":8081", proxy)
+	proxy := &httputil.ReverseProxy{Director: director}
+
+	
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	})
+
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
